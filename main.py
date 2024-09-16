58d9339ba6e7bcf79fe4ed257df7fec21ff9eb33
Code32123 <jimmy.l.smythe@gmail.com>
2024-09-16 15:02:21 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, Response
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import threading
+import time
+import json
+
+app = Flask(__name__)
+# auth = HTTPBasicAuth()
+# users = {
+    # "usr": generate_password_hash("pass"),
+# }
+
+# @auth.verify_password
+# def verify_password(username, password):
+#     if username in users and check_password_hash(users.get(username), password):
+#         return username
+
+try:
+    with open("config.json", "r") as f:
+        cfg = json.load(f)
+except FileNotFoundError:
+    cfg = {"IPS": [["local", "127.0.0.1:3545"]]}
+    with open("config.json", "w") as f:
+        json.dump(cfg, f)
+
+@app.route("/")
+# @auth.login_required
+def hello_world():
+    with open("index.html", "r") as f:
+        file = f.read().replace("{{CFG}}", str(cfg))
+    return file
+
+@app.route("/RobotoMono-Regular.ttf")
+def font():
+    with open("RobotoMono-Regular.ttf", "rb") as f:
+        file = Response(f.read(), mimetype='font/tff')
+    return file
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=3546)
